4f0aacc67d4967a7c29b3d76cffc59d196757c9a
Wolfgang Imig <wolfgang.imig@googlemail.com>
2014-12-05 15:34:48 +0100

    Added a version.txt

--- java/joa/src/com/wilutions/joa/swing/package-info.java
+++ java/joa/src/com/wilutions/joa/swing/package-info.java
@@ -1,5 +1,7 @@
 /**
  * This package allows to use Java Swing in an Add-in.
- * The package is still in experimental state. It was found, that JMenuBar components cannot be used. Other GUI components seem to be OK.
+ * It was found, that JMenuBar components cannot be integrated. 
+ * This is because a JMenuBar has to be attached to a JFrame. 
+ * But JOA does only provide a WEmbeddedFrame to host content.  
  */
 package com.wilutions.joa.swing;
